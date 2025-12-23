3524b1d4381222ab08d798cca720c2d03cc0c91a
Shashwat Singh <shashwat98k@gmail.com>
2025-12-23 22:56:44 +0530

    Update amazon_brazil_market_analysis.sql

--- script/amazon_brazil_market_analysis.sql
+++ script/amazon_brazil_market_analysis.sql
@@ -94,10 +94,10 @@ DQL Script: Analysis - I
 
 Script Purpose:
 ========================================================================================
-	1. To simplify its financial reports, Amazon India needs to standardize payment values.
-	   Display the average payment values for each payment_type.
-  	   Round the average payment values to integer (no decimal)
-       and display the results sorted in ascending order. 
+	1. To simplify its financial reports, Amazon India needs to standardize 
+	   payment values. Display the average payment values for each payment_type.
+	   Round the average payment values to integer (no decimal) and display the 
+	   results sorted in ascending order. 
 */
 
 SELECT 
@@ -109,25 +109,30 @@ GROUP BY payment_type
 ORDER BY rounded_avg_payment ASC;
 /*
 ========================================================================================
-	2. To refine its payment strategy, 
-	   Amazon India wants to know the distribution of orders by payment type. 
-	   Calculate the percentage of total orders for each payment type, 
-	   rounded to one decimal place, and display them in descending order
+	2. To refine its payment strategy, Amazon India wants to know the 
+	   distribution of orders by payment type. Calculate the percentage 
+	   of total orders for each payment type, rounded to one decimal place, 
+	   and display them in descending order
 */
 
 SELECT payment_type,
-       ROUND(COUNT(DISTINCT order_id)*100.0/
-       (SELECT COUNT(DISTINCT order_id)
-        FROM amazon_brazil.payments
-        WHERE payment_type <> 'not_defined'),1) AS percentage_orders
-FROM amazon_brazil.payments
-WHERE payment_type <> 'not_defined'
-GROUP BY payment_type
+       ROUND(COUNT(DISTINCT o.order_id)*100.0/
+       (SELECT COUNT(DISTINCT o.order_id)
+        FROM amazon_brazil.payments p 
+		JOIN amazon_brazil.orders o 
+			ON p.order_id = o.order_id
+        WHERE p.payment_type <> 'not_defined'),1) AS percentage_orders
+FROM amazon_brazil.payments p 
+JOIN amazon_brazil.orders o 
+	ON p.order_id = o.order_id
+WHERE p.payment_type <> 'not_defined'
+GROUP BY p.payment_type
 ORDER BY percentage_orders DESC;
 /*
 ========================================================================================
-	3. Amazon India seeks to create targeted promotions for products within specific price ranges. 
-   	   Identify all products priced between 100 and 500 BRL that contain the word 'Smart' in their name. 
+	3. Amazon India seeks to create targeted promotions for products within
+	   specific price ranges. Identify all products priced between 100 and 
+	   500 BRL that contain the word 'Smart' in their name. 
 	   Display these products, sorted by price in descending order.
 */
 
@@ -143,10 +148,9 @@ GROUP BY oi.product_id
 ORDER BY price DESC;
 /*
 ========================================================================================
-	4.To identify seasonal sales patterns, 
-	  Amazon India needs to focus on the most successful months. 
-	  Determine the top 3 months with the highest total sales value, 
-	  rounded to the nearest integer.
+	4. To identify seasonal sales patterns, Amazon India needs to focus on
+	   the most successful months. Determine the top 3 months with the 
+	   highest total sales value, rounded to the nearest integer.
 */
 
 SELECT 
@@ -162,9 +166,9 @@ LIMIT 3;
 
 /*
 ========================================================================================
-	5. Amazon India is interested in product categories with significant price variations. 
-	   Find categories where the difference between the maximum and 
-	   minimum product prices is greater than 500 BRL.	   
+	5. Amazon India is interested in product categories with significant 
+	   price variations. Find categories where the difference between 
+	   the maximum and minimum product prices is greater than 500 BRL.	   
 */
 
 SELECT 
@@ -179,9 +183,9 @@ HAVING MAX(oi.price) - MIN(oi.price) > 500
 ORDER BY price_difference DESC;
 /*
 ========================================================================================
-	6. To enhance the customer experience, 
-	   Amazon India wants to find which payment types have the most consistent transaction amounts. 
-	   Identify the payment types with the least variance in transaction amounts, 
+	6. To enhance the customer experience, Amazon India wants to find which
+	   payment types have the most consistent transaction amounts. Identify 
+	   the payment types with the least variance in transaction amounts, 
 	   sorting by the smallest standard deviation first.   
 */
 
@@ -194,9 +198,10 @@ GROUP BY payment_type
 ORDER BY std_deviation ASC;
 /*
 ========================================================================================
-	7. Amazon India wants to identify products that may have incomplete name in order to 
-	   fix it from their end. Retrieve the list of products where the product category 
-	   name is missing or contains only a single character.  
+	7. Amazon India wants to identify products that may have incomplete 
+	   name in order to fix it from their end. Retrieve the list of products 
+	   where the product category name is missing or contains only a single
+	   character.  
 */
 
 SELECT 
@@ -212,7 +217,6 @@ DQL Script: Analysis - II
 
 Script Purpose:
 ========================================================================================
-
 	1. Amazon India wants to understand which payment types are most popular across 
 	   different order value segments (e.g., low, medium, high). Segment order values 
 	   into three ranges: orders less than 200 BRL, between 200 and 1000 BRL, and 
@@ -233,9 +237,227 @@ SELECT
 	p.payment_type,
 	COUNT(DISTINCT ov.order_id) AS count
 FROM order_value_table ov
-JOIN amazon_brazil.payments p ON ov.order_id = p.order_id
+JOIN amazon_brazil.payments p 
+	ON ov.order_id = p.order_id
 WHERE p.payment_type <> 'not_defined'
 GROUP BY order_value_segment,p.payment_type
-ORDER BY order_value_segment,count DESC;
+ORDER BY count DESC;
 /*
 ========================================================================================
+	2. Amazon India wants to analyse the price range and average price for 
+	   each product category. Calculate the minimum, maximum, and average 
+	   price for each category, and list them in descending order by the 
+	   average price.*/
+	   
+SELECT 
+	p.product_category_name,
+	MIN(oi.price) AS min_price,
+	MAX(oi.price) AS max_price,
+	ROUND(AVG(oi.price),2) AS avg_price
+FROM amazon_brazil.order_items oi
+JOIN amazon_brazil.products p 
+	ON oi.product_id = p.product_id
+GROUP BY p.product_category_name
+ORDER BY avg_price DESC;
+/*
+========================================================================================
+	3. Amazon India wants to identify the customers who have placed multiple 
+	   orders over time. Find all customers with more than one order, and 
+	   display their customer unique IDs along with the total number of 
+	   orders they have placed.*/
+
+SELECT 
+	c.customer_unique_id,
+	COUNT(DISTINCT o.order_id) AS total_orders
+FROM amazon_brazil.customers c
+JOIN amazon_brazil.orders o
+	ON o.customer_id = c.customer_id
+GROUP BY c.customer_unique_id
+HAVING COUNT(*) > 1
+ORDER BY total_orders DESC;
+/*
+========================================================================================
+	4. Amazon India wants to categorize customers into different types 
+	   ('New – order qty. = 1' ;  'Returning' –order qty. 2 to 4;  'Loyal' – order qty. >4) 
+	   based on their purchase history. Use a temporary table to define these categories and 
+	   join it with the customers table to update and display the customer types.*/
+	   
+CREATE TEMP TABLE customer_type AS
+SELECT 
+	customer_id,
+	CASE WHEN total_orders = 1 THEN 'New'
+		 WHEN total_orders BETWEEN 2 AND 4 THEN 'Returning'
+		 ELSE 'Loyal'
+	END AS customer_type
+FROM(
+	SELECT 
+	customer_id,
+	COUNT(DISTINCT order_id) AS total_orders
+FROM amazon_brazil.orders
+GROUP BY customer_id
+) t;
+
+SELECT 
+    c.customer_unique_id,
+    ct.customer_type
+FROM amazon_brazil.customers c
+JOIN customer_type ct
+    ON c.customer_id = ct.customer_id;
+/*
+========================================================================================
+	5. Amazon India wants to know which product categories generate the most revenue.
+	   Use joins between the tables to calculate the total revenue for each product 
+	   category. Display the top 5 categories.
+*/
+	   
+SELECT 
+	p.product_category_name,
+	ROUND(SUM(oi.price + oi.freight_value),0) AS total_revenue
+FROM amazon_brazil.orders o 
+JOIN amazon_brazil.order_items oi
+	ON o.order_id = oi.order_id
+JOIN amazon_brazil.products p
+	ON oi.product_id = p.product_id
+WHERE o.order_status NOT IN ('canceled', 'unavailable')
+GROUP BY p.product_category_name
+ORDER BY total_revenue DESC
+LIMIT 5;
+/*
+========================================================================================
+DQL Script: Analysis - III
+
+Script Purpose:
+========================================================================================
+	1. The marketing team wants to compare the total sales between different seasons. 
+	   Use a subquery to calculate total sales for each season 
+	   (Spring, Summer, Autumn, Winter) based on order purchase dates, 
+	   and display the results. Spring is in the months of March, April and May. 
+	   Summer is from June to August and Autumn is between September and November and 
+	   rest months are Winter.
+*/
+
+SELECT 
+	CASE WHEN DATE_PART('month',order_purchase_timestamp) IN (3,4,5) THEN 'Spring'
+		 WHEN DATE_PART('month',order_purchase_timestamp) IN (6,7,8) THEN 'Summer'
+		 WHEN DATE_PART('month',order_purchase_timestamp) IN (9,10,11) THEN 'Autumn'
+		 ELSE 'Winter'
+		 END AS season,
+	ROUND(SUM(oi.price + oi.freight_value),0) AS total_sales
+FROM amazon_brazil.orders o 
+JOIN amazon_brazil.order_items oi
+	ON o.order_id = oi.order_id
+WHERE o.order_status NOT IN ('canceled', 'unavailable')
+GROUP BY season
+ORDER BY total_sales DESC
+/*
+========================================================================================
+	2 . The inventory team is interested in identifying products that have sales 
+	    volumes above the overall average. Write a query that uses a subquery to 
+		filter products with a total quantity sold above the average quantity.
+*/
+
+WITH quantity_sold AS (
+SELECT 
+	p.product_id,
+	COUNT(*) AS total_quantity_sold
+FROM amazon_brazil.orders o 
+JOIN amazon_brazil.order_items oi
+	ON o.order_id = oi.order_id
+JOIN amazon_brazil.products p
+	ON oi.product_id = p.product_id
+WHERE o.order_status NOT IN ('canceled', 'unavailable')
+GROUP BY p.product_id
+)
+SELECT 
+	product_id,
+	total_quantity_sold
+FROM quantity_sold
+WHERE total_quantity_sold > (
+SELECT 
+	AVG(total_quantity_sold) AS avg_total_quantity_sold
+FROM quantity_sold
+)
+/*
+========================================================================================
+	3 . To understand seasonal sales patterns, the finance team is analysing 
+		the monthly revenue trends over the past year (year 2018). Run a query to
+		calculate total revenue generated each month and identify periods of peak 
+		and low sales. Export the data to Excel and create a graph to visually 
+		represent revenue changes across the months. 
+*/
+
+SELECT 
+    TO_CHAR(month, 'Mon YYYY') AS month,
+    total_revenue
+FROM (
+    SELECT 
+        DATE_TRUNC('month', o.order_purchase_timestamp) AS month,
+        ROUND(SUM(oi.price + oi.freight_value), 0) AS total_revenue
+    FROM amazon_brazil.orders o
+    JOIN amazon_brazil.order_items oi
+        ON oi.order_id = o.order_id
+    WHERE o.order_status NOT IN ('canceled', 'unavailable')
+      AND DATE_PART('year', o.order_purchase_timestamp) = 2018
+    GROUP BY DATE_TRUNC('month', o.order_purchase_timestamp)
+	ORDER BY month
+) t
+/*
+========================================================================================
+	4 . A loyalty program is being designed  for Amazon India. Create a 
+		segmentation based on purchase frequency: ‘Occasional’ for customers
+		with 1-2 orders, ‘Regular’ for 3-5 orders, and ‘Loyal’ for more than
+		5 orders. Use a CTE to classify customers and their count and generate
+		a chart in Excel to show the proportion of each segment.
+*/
+WITH CTE AS (
+SELECT 
+	customer_id,
+	CASE WHEN total_orders <= 2 THEN 'Occasional'
+		 WHEN total_orders BETWEEN 3 AND 5 THEN 'Regular'
+		 ELSE 'Loyal'
+	END AS customer_type
+FROM(
+	SELECT 
+	customer_id,
+	COUNT(DISTINCT order_id) AS total_orders
+FROM amazon_brazil.orders
+GROUP BY customer_id
+) t
+)
+SELECT 
+	customer_type,
+	COUNT(*) AS count
+FROM CTE
+GROUP BY customer_type
+
+/*
+========================================================================================
+	5 . Amazon wants to identify high-value customers to target for 
+	an exclusive rewards program. You are required to rank customers 
+	based on their average order value (avg_order_value) to find the
+	top 20 customers.
+*/
+
+
+SELECT 
+	o.customer_id ,
+	ROUND(AVG(oi.price + oi.freight_value),2) AS avg_order_value,
+	RANK()OVER(ORDER BY AVG(oi.price + oi.freight_value) DESC) AS customer_rank
+FROM amazon_brazil.order_items oi
+JOIN amazon_brazil.orders o 
+	ON oi.order_id = o.order_id
+WHERE o.order_status NOT IN ('canceled', 'unavailable')
+GROUP BY o.customer_id
+ORDER BY customer_rank
+LIMIT 20;
+
+/*
+========================================================================================
+	6 . Amazon wants to analyze sales growth trends for its key products
+		over their lifecycle. Calculate monthly cumulative sales for each 
+		product from the date of its first sale. Use a recursive CTE to 
+		compute the cumulative sales (total_sales) for each product month 
+		by month.
+
+    Output: product_id, sale_month, and total_sales
+*/
